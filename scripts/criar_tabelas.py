46110804a7b13793a08bd07daa6da29c7b471ffb
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-04-16 16:19:36 -0300

    � fix(import): ajusta sys.path para permitir importação do app

--- scripts/criar_tabelas.py
+++ scripts/criar_tabelas.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 from app.models.modelos import db, Status, TipoDemanda, RegiaoAdministrativa, Demanda
 
