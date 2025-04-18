dc354eeace4ffcef7678821e2c2ce7a4b41de12b
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2025-04-17 21:44:35 -0300

    Corrige importação no script verificar_usuarios.py com ajuste no sys.path

--- scripts/verificar_usuarios.py
+++ scripts/verificar_usuarios.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app
 from app.ext import db
 from app.models.modelos import Usuario
