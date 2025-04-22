d1fb30a0d6cb49552901ed227fdbb3af9f5062be
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-04-22 09:27:02 -0300

    Ajusta execução do Flask para servidor acessível via rede (host 0.0.0.0)

--- run.py
+++ run.py
@@ -792,4 +792,4 @@ def ver_atendimento(id):
 # Execução do servidor
 # ================================
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
