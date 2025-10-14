80a34a8b3c865edb17107ad75873d71574ac67dc
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-10-14 15:30:41 -0300

    feat: corrige o diretório

--- app/protocolo/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/protocolo/__init__.py
-"""
-Blueprint de Protocolo (atendimentos e interações)
-"""
-from flask import Blueprint
-
-protocolo_bp = Blueprint('protocolo_bp', __name__)
-
-from app.protocolo import routes  # noqa: E402,F401
