17b3104fd464bc04446b5ae4b195d70140236883
Anderson de Matos Guimarães <anderson.guimaraes@novacap.df.gov.br>
2025-12-02 15:17:59 -0300

    feat: migração db

--- run.py
+++ run.py
@@ -15,4 +15,4 @@ app = create_app()
 # ⚙ Execução do servidor (modo desenvolvimento)
 # ==================================================
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
